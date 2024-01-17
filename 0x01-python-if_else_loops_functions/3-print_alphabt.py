13f0b5529f9db9986c00cfdb169f1af44a6f1587
hallomer <hebaaltayeb2@icloud.com>
2024-01-16 17:46:40 -0800

    :)

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-print(f"{''.join(chr(i) for i in range(97, 123) if i not in [101, 113])}", end="")
-
+for i in range(97, 123):
+    if i != 113:
+        if i != 101:
+            print('{}'.format(chr(i)), end='')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-	for j in range(i + 1, 10):
-		print(f"{i}{j}", end=", " if i < 8 else "\n")
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        if i != j:
+            print("{}{}, ".format(i, j), end='')
+print("89")
