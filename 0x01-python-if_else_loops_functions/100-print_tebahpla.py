bb5be39efeb309ab4851c9679bcaae2356fdf259
hallomer <hebaaltayeb2@icloud.com>
2024-01-16 18:10:29 -0800

    bye bye

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    print(f"{chr(i)}{chr(i - 32)}", end='')
+for i in range(122, 97, -2):
+    print("{}{}".format(chr(i), chr(i - 33)), end='')
