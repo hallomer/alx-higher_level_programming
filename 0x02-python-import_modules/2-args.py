dccc956a395b0b78ded6465b58c7e520e6bb02bf
hallomer <hebaaltayeb2@icloud.com>
2024-01-18 07:08:31 -0800

    sorry, pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-     from sys import argv
-     n = len(argv)
-     if n == 1:
-          print("0 arguments.")
-     elif n == 2:
-          print("{} argument:".format(n - 1))
-          print("1: {}".format(argv[1]))
-     else:
-          print("{} arguments:".format(n - 1))
-          for x in range(1, n):
-               print("{}: {}".format(x, argv[x]))
+    from sys import argv
+    n = len(argv)
+    if n == 1:
+        print("0 arguments.")
+    elif n == 2:
+        print("{} argument:".format(n - 1))
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(n - 1))
+        for x in range(1, n):
+            print("{}: {}".format(x, argv[x]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-     from sys import argv
-     n = len(argv)
-     if n < 3:
-          pass
-     else:
-          sum = 0
-          for x in range(1, n):
-               sum = sum + int(argv[x])
-          print(sum)
+    from sys import argv
+    n = len(argv)
+    sum = 0
+    for x in range(1, n):
+        sum += int(argv[x])
+    print(sum)
