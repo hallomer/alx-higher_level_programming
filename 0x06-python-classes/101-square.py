380991e26a87a63cba33241387183ac40657814b
Hiba <hebaaltayeb2@icloud.com>
2024-02-07 02:13:54 +0300

    docs

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -90,29 +90,29 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        """
-        Prints a visual representation of the square.
-        """
+        """Prints a visual representation of the square."""
         if self.__size == 0:
-            print()
-        else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
 
     def __str__(self):
         """
         Returns a string representation of the square.
 
         Returns:
-            str: The string representation of the square.
+        str: The string representation of the square.
         """
-        if self.__size == 0:
-            return ""
-        square_str = ""
-        for _ in range(self.__position[1]):
-            square_str += "\n"
-        for _ in range(self.__size):
-            square_str += " " * self.__position[0] + "#" * self.__size + "\n"
-        return square_str
+        if self.__size != 0:
+            [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            if i != self.__size - 1:
+                print("")
+        return ("")
