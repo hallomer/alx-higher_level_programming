5cb5683917a273be1fedc7381c7995c67795fffc
Hiba <hebaaltayeb2@icloud.com>
2024-02-07 01:46:09 +0300

    docs

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-import math
-
 """this is a magic class"""
+import math
 
 
 class MagicClass:
-    """
-    Represents a magical shape with a radius.
-    """
+    """Represents a magical shape with a radius."""
 
     def __init__(self, radius=0):
-        """
-        Initializes a new instance of the MagicClass.
-        """
+        """Initializes a new instance of the MagicClass."""
         if not isinstance(radius, (int, float)):
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """
-        Calculates and returns the area of the magical shape.
-        """
+        """Calculates and returns the area of the magical shape."""
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-        """
-        Calculates and returns the circumference of the magical shape.
-        """
+        """Calculates and returns the circumference of the magical shape."""
         return 2 * math.pi * self.__radius
