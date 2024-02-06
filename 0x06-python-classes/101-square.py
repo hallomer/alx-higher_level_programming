583a67df8c855a3c54700f2a888126fff2c1b86f
Hiba <hebaaltayeb2@icloud.com>
2024-02-07 02:07:25 +0300

    docs

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -75,7 +75,8 @@ class Square:
             TypeError: If the value is not a tuple of 2 positive integers.
         """
         if not isinstance(value, tuple) or len(value) != 2 or \
-           not all(isinstance(i, int) and i >= 0 for i in value):
+           not all(isinstance(i, int) and i >= 0 for i in value) or \
+           not all(num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
