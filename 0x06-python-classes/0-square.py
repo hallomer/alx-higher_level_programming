5823367f64a612d571b52f6f9ee8d1a0a665dffd
Hiba <hebaaltayeb2@icloud.com>
2024-02-07 01:28:02 +0300

    halo

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+''' Empty class'''
 
 class Square:
-    """
+    '''
     The Square class represents a square shape.
 
     This class doesn't have any attributes or methods defined yet.
     It serves as a placeholder for future functionality related to squares.
-    """
+    '''
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 class Square:
-    """
+    '''
     Represents a square shape with a size.
 
     Attributes:
@@ -9,13 +9,13 @@ class Square:
 
     Methods:
         __init__(size): Initializes a new instance of the Square class.
-    """
+    '''
 
     def __init__(self, size):
-        """
+        '''
         Initializes a new instance of the Square class.
 
         Args:
             size (int): The size of the square.
-        """
+        '''
         self.__size = size
