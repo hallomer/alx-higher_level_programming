2b1abc64cddac3a691912fd59db7240965ebbba3
Hiba <hebaaltayeb2@icloud.com>
2024-03-27 04:09:25 +0300

    halo

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- deletes the database hbtn_0c_0
+DROP database if EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- creates a table called first_table in the current database
+CREATE table if NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
