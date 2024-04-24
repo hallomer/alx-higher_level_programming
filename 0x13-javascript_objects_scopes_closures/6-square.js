febc31e594cf6c389603b74c9061ed44d1da1c9a
Hiba <hebaaltayeb2@icloud.com>
2024-04-24 03:48:56 +0200

    javaScript

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,6 @@
 const ParentSquare = require('./5-square');
 
 module.exports = class Square extends ParentSquare {
-  constructor (...args) {
-    super(...args);
-  }
-
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
