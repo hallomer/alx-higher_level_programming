c78dfcc5be25c8da099250d28a23a46a4c42c314
Hiba <hebaaltayeb2@icloud.com>
2024-04-27 07:20:14 +0200

    4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Lists all cities from the db hbtn_0e_4_usa."""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", post=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.id cities.name states.name FROM cities\
+                   JOIN states ON cities.states_id = states.id\
+                   ORDER BY cities.id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
