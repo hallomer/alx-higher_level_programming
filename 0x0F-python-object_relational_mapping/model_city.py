a777b19f922a6361ce7bd6f80b14c5adc75ad598
Hiba <hebaaltayeb2@icloud.com>
2024-04-27 08:39:07 +0200

    1314

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""
+Contains the class definition of a City.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+class City(Base):
+    """
+    City class inherits from Base,
+    links to the MySQL table cities
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State", back_populates="cities")
+
+    def __repr__(self):
+        """
+        String representation of a City object.
+        """
+        return "<City(id='{}', name='{}', state_id='{}')>".format(
+            self.id, self.name, self.state_id)
