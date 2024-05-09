100ca67b23349de82403ce66c1b239605402336b
Hiba <hebaaltayeb2@icloud.com>
2024-05-09 10:16:54 -0700

    6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 """Finds a peak in a list of unsorted integers."""
+
+
 def find_peak(list_of_integers):
+    """Finds a peak."""
     if not list_of_integers or list_of_integers == []:
         return None
-    return max(list_of_integers)
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            left = mid + 1
+        else:
+            right = mid
+
+    return list_of_integers[left]
