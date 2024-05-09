ce24a226e7d01587a98498cae5af9f953839027e
Hiba <hebaaltayeb2@icloud.com>
2024-05-09 17:30:20 +0200

    3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all HTTP methods the server will accept
-curl -sI "$1" | grep -i Allow | awk '{gsub(",", "\n"); print substr($0, index($0,$2))}
+curl -sI "$1" | grep -i Allow | awk '{gsub(",", "\n"); print substr($0, index($0,$2))}'
