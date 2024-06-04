e995851373c7130cefc5dc81934b94fe9267cfd4
Hiba <hebaaltayeb2@icloud.com>
2024-06-05 01:11:39 +0300

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,7 +7,9 @@ request(url, (error, response, body) => {
     console.log(error);
   } else {
     const films = JSON.parse(body).results;
-    const wedgeCount = films.filter(film => film.characters.includes('/18/')).length;
+    const wedgeCount = films.filter(film =>
+      film.characters.some(characterUrl => characterUrl.includes('/18/'))
+    ).length;
     console.log(wedgeCount);
   }
 });
